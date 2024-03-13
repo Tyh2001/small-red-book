f2de96d4d3c75b9edebb0c4303f4bfe0e7239d6f
Tyh2001 <1469442737@qq.com>
2024-03-13 14:50:32 +0800

    Initial Commit

---
+++ small-red-book/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  small-red-book
+//
+//  Created by 田宇浩 on 2024/3/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ small-red-book/small_red_bookApp.swift
@@ -0,0 +1,17 @@
+//
+//  small_red_bookApp.swift
+//  small-red-book
+//
+//  Created by 田宇浩 on 2024/3/13.
+//
+
+import SwiftUI
+
+@main
+struct small_red_bookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
